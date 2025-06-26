ec96a9183100988c503ba3ebce0b304ca05e7b35
Łukasz Duda <lukasz.duda.pl@gmail.com>
2025-06-26 20:30:42 +0200

    Correct name charset

--- api/Migrations/20250626113003_CorrectCharset.Designer.cs
+++ api/Migrations/20250626113003_CorrectCharset.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Home.Shopping.Infrastructure;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Home.Shopping.Migrations
 {
     [DbContext(typeof(ShoppingDbContext))]
-    partial class ShoppingDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250626113003_CorrectCharset")]
+    partial class CorrectCharset
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ api/Migrations/20250626113003_CorrectCharset.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Home.Shopping.Migrations
+{
+    /// <inheritdoc />
+    public partial class CorrectCharset : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"ALTER TABLE MatchFragment
+MODIFY `Name` longtext CHARACTER SET utf8mb4;
+ALTER TABLE MatcherGroups
+MODIFY `Name` longtext CHARACTER SET utf8mb4;
+ALTER TABLE ShoppingListItems
+MODIFY `Name` longtext CHARACTER SET utf8mb4;");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
